a538a906bed87758c3135faa2a82a3640642ad61
bing <1466371706@qq.com>
2019-05-25 20:08:30 +0800

    实现

--- src/main/java/com/thoughtworks/tdd/Dependency.java
+++ src/main/java/com/thoughtworks/tdd/Dependency.java
@@ -2,6 +2,29 @@ package com.thoughtworks.tdd;
 
 public class Dependency {
     public String say(){
+        for (int i = 0; i <= 120; i++){
+            if(i % 3 == 0){
+                if(i % 5 == 0){
+                    System.out.println("FizzBuzz");
+                }else if(i % 7 == 0){
+                    System.out.println("FizzWhizz");
+                }else if(i % 5 == 0 && i % 5 == 0){
+                    System.out.println("FizzBuzzWhizz");
+                }else {
+                    System.out.println("Fizz");
+                }
+            }else if(i % 5 == 0){
+                if(i % 7 == 0){
+                    System.out.println("BuzzWhizz");
+                }else{
+                    System.out.println("Buzz");
+                }
+            }else if(i % 7 == 0){
+                System.out.println("Whizz");
+            }else{
+                System.out.println(i);
+            }
+        }
         return "Leave me alone.";
     }
 }
